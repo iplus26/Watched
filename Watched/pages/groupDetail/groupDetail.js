b14c744dda2431ab01a17228eec643a82faca824
iplus26 <iplus26@gmail.com>
2015-03-10 15:10:31 +0800

    init

---
+++ Watched/pages/groupDetail/groupDetail.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+
+    ui.Pages.define("/pages/groupDetail/groupDetail.html", {
+        /// <field type="WinJS.Binding.List" />
+        _items: null,
+
+        // This function is called to initialize the page.
+        init: function (element, options) {
+            var group = Data.resolveGroupReference(options.groupKey);
+            this._items = Data.getItemsFromGroup(group);
+            this._pageTitle = group.title;
+            var pageList = this._items.createGrouped(
+                function groupKeySelector(item) { return group.key; },
+                function groupDataSelector(item) { return group; }
+            );
+            this.groupDataSource = pageList.groups.dataSource;
+            this.itemDataSource = pageList.dataSource;
+            this.itemInvoked = ui.eventHandler(this._itemInvoked.bind(this));
+        },
+
+        // This function is called whenever a user navigates to this page. 
+        ready: function (element, options) {
+            element.querySelector("header[role=banner] .pagetitle").textContent = this._pageTitle;
+        },
+
+        unload: function () {
+            this._items.dispose();
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        _itemInvoked: function (args) {
+            var item = this._items.getAt(args.detail.itemIndex);
+            WinJS.Navigation.navigate("/pages/itemDetail/itemDetail.html", { item: Data.getItemReference(item) });
+        }
+    });
+})();
