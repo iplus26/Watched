4edd4a1ea5666fe3560d3e9c3168b2906996ad1c
iplus26 <iplus26@gmail.com>
2015-03-10 19:38:16 +0800

    add cool icon

---
+++ Watched/bin/Debug/AppX/pages/groupedItems/groupedItems.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+    var ui = WinJS.UI;
+
+    ui.Pages.define("/pages/groupedItems/groupedItems.html", {
+        // This function is called to initialize the page.
+        init: function (element, options) {
+            this.groupHeaderInvoked = ui.eventHandler(this._groupHeaderInvoked.bind(this));
+            this.itemInvoked = ui.eventHandler(this._itemInvoked.bind(this));
+        },
+
+        // This function is called whenever a user navigates to this page.
+        ready: function (element, options) {
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        _groupHeaderInvoked: function (args) {
+            var group = Data.groups.getAt(args.detail.groupHeaderIndex);
+            nav.navigate("/pages/groupDetail/groupDetail.html", { groupKey: group.key });
+        },
+
+        _itemInvoked: function (args) {
+            var item = Data.items.getAt(args.detail.itemIndex);
+            nav.navigate("/pages/itemDetail/itemDetail.html", { item: Data.getItemReference(item) });
+        }
+    });
+})();
