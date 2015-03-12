c235c81f510163a0df3c1a2f50bb79c83c12ee11
iplus26 <iplus26@gmail.com>
2015-03-12 12:27:23 +0800

    3-12 12:27 PM a lot code..

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # todo
-* **urgent & important:** 获得豆瓣的 advance 权限，获取豆瓣口碑榜（小虚）
-* **important:** 电影页面，use Hub UI. (ME)
+* **important & urgent:** 电影页面，use Hub UI. (ME)
 	* 豆瓣的基本信息，IMDb 信息等
 	* 跳转播放预告片、在线观看等
 * **important:** Charms Bar Search（精哥）
-* Charms Bar Share. [API](https://msdn.microsoft.com/en-us/library/windows/apps/windows.applicationmodel.datatransfer.datatransfermanager.showshareui.aspx)
-* 用豆瓣的 advance 权限标记电影，获取关注人的短评（权限？）
+* **important:** * 用豆瓣的 advance 权限标记电影（小虚），获取关注人的短评
+* Charms Bar Share. [Document](https://msdn.microsoft.com/en-us/library/windows/apps/windows.applicationmodel.datatransfer.datatransfermanager.showshareui.aspx)
 * 影人页面
 * 剧集页面（剧集使用 Trakt.tv 的 API 获取剧集更新，监测 kickass 的资源更新，在线播放等）
 * 「看过」的更多功能
+
+# update
+
+2015.03.11
+
+* add 豆瓣电影口碑榜
+
+2015.03.10
+
+* groupDetail Page
+
+2015.03.09
+
+* groupedItems Page
+
---
+++ Watched/bin/Debug/AppX/pages/searchResult/searchResults.js
@@ -0,0 +1,146 @@
+﻿// TODO: Connect the Search Results Page to your in-app search.
+// For an introduction to the Search Results Page template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232512
+(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+    var utils = WinJS.Utilities;
+
+    WinJS.UI.Pages.define("/pages/searchResult/searchResults.html", {
+        _filters: [],
+        _lastSearch: "",
+
+        // This function is called to initialize the page.
+        init: function (element, options) {
+            this.itemInvoked = ui.eventHandler(this._itemInvoked.bind(this));
+        },
+
+        // This function is called whenever a user navigates to this page.
+        ready: function (element, options) {
+            var listView = element.querySelector(".resultslist").winControl;
+            this._handleQuery(element, options);
+            listView.element.focus();
+        },
+
+        // This function updates the page layout in response to layout changes.
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        // This function filters the search data using the specified filter.
+        _applyFilter: function (filter, originalResults) {
+            if (filter.results === null) {
+                filter.results = originalResults.createFiltered(filter.predicate);
+            }
+            return filter.results;
+        },
+
+        // This function responds to a user selecting a new filter. It updates the
+        // selection list and the displayed results.
+        _filterChanged: function (element, filterIndex) {
+            var filterBar = element.querySelector(".filterbar");
+            var listView = element.querySelector(".resultslist").winControl;
+
+            utils.removeClass(filterBar.querySelector(".highlight"), "highlight");
+            utils.addClass(filterBar.childNodes[filterIndex], "highlight");
+
+            listView.itemDataSource = this._filters[filterIndex].results.dataSource;
+        },
+
+        _generateFilters: function () {
+            this._filters = [];
+            this._filters.push({ results: null, text: "All", predicate: function (item) { return true; } });
+
+            // TODO: Replace or remove example filters.
+            this._filters.push({ results: null, text: "Group 1", predicate: function (item) { return item.group.key === "group1"; } });
+            this._filters.push({ results: null, text: "Group 2+", predicate: function (item) { return item.group.key !== "group1"; } });
+        },
+
+        // This function executes each step required to perform a search.
+        _handleQuery: function (element, args) {
+            var originalResults;
+            this._lastSearch = args.queryText;
+            WinJS.Namespace.define("searchResults", { markText: WinJS.Binding.converter(this._markText.bind(this)) });
+            this._initializeLayout(element);
+            this._generateFilters();
+            originalResults = this._searchData(args.queryText);
+            if (originalResults.length === 0) {
+                document.querySelector('.filterbar').style.display = "none";
+            } else {
+                document.querySelector('.resultsmessage').style.display = "none";
+            }
+            this._populateFilterBar(element, originalResults);
+            this._applyFilter(this._filters[0], originalResults);
+        },
+
+        _initializeLayout: function (element) {
+            element.querySelector(".titlearea .pagetitle").textContent = "看过 Watched";
+            element.querySelector(".titlearea .pagesubtitle").textContent = "“" + this._lastSearch + '”的搜索结果';
+        },
+
+        _itemInvoked: function (args) {
+            args.detail.itemPromise.done(function itemInvoked(item) {
+                // TODO: Navigate to the item that was invoked.
+                // 导航到对应电影的页面 itemDetailPage.html
+            });
+        },
+
+        // This function colors the search term. Referenced in /pages/searchResult/searchResults.html
+        // as part of the ListView item templates.
+        _markText: function (text) {
+            return text.replace(this._lastSearch, "<mark>" + this._lastSearch + "</mark>");
+        },
+
+        // This function generates the filter selection list.
+        _populateFilterBar: function (element, originalResults) {
+            var filterBar = element.querySelector(".filterbar");
+            var listView = element.querySelector(".resultslist").winControl;
+            var li, option, filterIndex;
+
+            filterBar.innerHTML = "";
+            for (filterIndex = 0; filterIndex < this._filters.length; filterIndex++) {
+                this._applyFilter(this._filters[filterIndex], originalResults);
+
+                li = document.createElement("li");
+                li.filterIndex = filterIndex;
+                li.tabIndex = 0;
+                li.textContent = this._filters[filterIndex].text + " (" + this._filters[filterIndex].results.length + ")";
+                li.onclick = function (args) { this._filterChanged(element, args.target.filterIndex); }.bind(this);
+                li.onkeyup = function (args) {
+                    if (args.key === "Enter" || args.key === "Spacebar")
+                        this._filterChanged(element, args.target.filterIndex);
+                }.bind(this);
+                utils.addClass(li, "win-type-interactive");
+                utils.addClass(li, "win-type-x-large");
+                filterBar.appendChild(li);
+
+                if (filterIndex === 0) {
+                    utils.addClass(li, "highlight");
+                    listView.itemDataSource = this._filters[filterIndex].results.dataSource;
+                }
+
+                option = document.createElement("option");
+                option.value = filterIndex;
+                option.textContent = this._filters[filterIndex].text + " (" + this._filters[filterIndex].results.length + ")";
+            }
+        },
+
+        // This function populates a WinJS.Binding.List with search results for the
+        // provided query.
+        _searchData: function (queryText) {
+            var originalResults;
+            // TODO: Perform the appropriate search on your data.
+            if (window.Data) {
+                originalResults = Data.items.createFiltered(function (item) {
+                    return (item.title.indexOf(queryText) >= 0 || item.subtitle.indexOf(queryText) >= 0 || item.description.indexOf(queryText) >= 0);
+                });
+            } else {
+                originalResults = new WinJS.Binding.List();
+            }
+            return originalResults;
+        }
+    });
+})();
